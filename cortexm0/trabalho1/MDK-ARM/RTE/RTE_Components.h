44acee5c8e5790a04d292f9aa005c712a3d24b3c
vabichequer <v.a.s.i@outlook.com>
2016-09-23 17:54:49 -0300

    First commit

---
+++ cortexm0/trabalho1/MDK-ARM/RTE/RTE_Components.h
@@ -0,0 +1,14 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'trabalho1' 
+ * Target:  'trabalho1 Configuration' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+#endif /* RTE_COMPONENTS_H */
---
+++ cortexm0/trabalho1/MDK-ARM/trabalho1 Configuration/startup_stm32f072xb.d
@@ -0,0 +1,1 @@
+trabalho1 configuration\startup_stm32f072xb.o: ../Drivers/CMSIS/Device/ST/STM32F0xx/Source/Templates/arm/startup_stm32f072xb.s
